4a901df87a23187d9f96984b87500411b075b643
shilpi <shilpikharya@gmail.com>
2021-03-13 09:35:39 -0800

    changing gulp to include gulp-clean-css to minify css and also keep unminified version

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,9 @@
 /* requiring all the three dependencies gulp, gulp-sass, setting the compiler node-sass */
-var {src, dest, watch} = require('gulp');
+var {src, dest, watch, series} = require('gulp');
 var sass = require('gulp-sass');
-
+const cleanCSS = require('gulp-clean-css');
 sass.compiler = require('node-sass');
+
 
 /* creating our task ,
 input will come from src folder, any sass file in folder .scss ,
@@ -12,7 +13,15 @@ function makeCSS () {
     return src('src/*.scss')
     .pipe(sass())
     .pipe(dest('dist/css'))
+    
 };
+
+function minifyCSS () {
+    return src('dist/css/*.css')
+    .pipe(cleanCSS({compatibility: 'ie8'}))
+    .pipe(dest('dist/css/minified_css'));
+
+}
 
 // exports.default = function() {
 //     watch('src/*.scss', makeCSS);
@@ -20,5 +29,8 @@ function makeCSS () {
 // }
 
 /* setting default task */
-exports.default = makeCSS;
-// const cleanCSS = require('gulp-clean-css'); // .pipe(cleanCSS({compatibility: 'ie8'}))
+//exports.default = makeCSS;
+//exports.default = minifyCSS;
+
+exports.default = series (makeCSS, minifyCSS);
+
